4f94d7149c8de4fd9af49fd6ee2bdd3a9ebc6765
Jakub Olejnik <olejnik.jakub71@gmail.com>
2021-03-16 22:31:44 +0100

    Added Bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
-import './App.global.css';
+import './styles/App.global.scss';
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <div className="buttons">
+        <button className="button is-primary" type="button">
+          Primary
+        </button>
+        <button className="button is-link" type="button">
+          Link
+        </button>
       </div>
     </div>
   );
