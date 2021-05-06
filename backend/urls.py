54a040836f9e562440d257c4e2b5433fe3255227
Aleksander Pucher <pucher.aleksander@gmail.com>
2021-05-06 22:20:37 +0200

    refactor shift view and create base for calendar_shift_view

--- backend/urls.py
+++ backend/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from .shifts.views import ShiftView
+from .shifts.views import ShiftCalendarSpecificView, ShiftView
 from .workers.views import WorkerView
 
 router = routers.DefaultRouter()
 router.register("workers", WorkerView)
 router.register("shifts", ShiftView)
+router.register("calendar", ShiftCalendarSpecificView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
