7b53e102e81f8a0814f407bf282a21c7be6b2500
Aleksander Pucher <pucher.aleksander@gmail.com>
2021-05-07 10:38:28 +0200

    overload get_Serializer_class method in shiftview

--- backend/shifts/views.py
+++ backend/shifts/views.py
@@ -17,53 +17,36 @@ def trydatetime(datetime_string):
     return True
 
 
+def return_drf_400_response_on_rangeparams_error(range_start, range_end):
+    if not (range_start and range_end):
+        return Response(
+            {"error": "'start' or/and 'end' query params are missing"},
+            status=400,
+        )
+    if not (trydatetime(range_start) and trydatetime(range_end)):
+        return Response(
+            {
+                "error": f"Your query params {range_start} or {range_end} do not match datetime format {settings.DATETIME_FORMAT}"
+            },
+            status=400,
+        )
+
+
 class ShiftView(viewsets.ModelViewSet):
     serializer_class = ShiftSerializer
     queryset = Shift.objects.all()
 
-    def list(self, request, *args, **kwargs):
-        start = request.query_params.get("start")
-        end = request.query_params.get("end")
-        if not (start and end):
-            return Response(
-                {"error": "'start' or/and 'end' query params are missing"},
-                status=400,
-            )
-        if not (trydatetime(start) and trydatetime(end)):
-            return Response(
-                {
-                    "error": f"Your query params {start} or {end} do not match datetime format {settings.DATETIME_FORMAT}"
-                },
-                status=400,
-            )
-
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(
-            queryset, many=True, context={"range_start": start, "range_end": end}
-        )
-
-        return Response(serializer.data)
-
-
-class ShiftCalendarSpecificView(viewsets.ModelViewSet):
-    serializer_class = ShiftCalendarSpecificSerializer
-    queryset = Shift.objects.all()
+    def get_serializer_class(self):
+        if self.request.query_params.get("calendar_specific"):
+            return ShiftCalendarSpecificSerializer
+        return super().get_serializer_class()
 
     def list(self, request, *args, **kwargs):
         start = request.query_params.get("start")
         end = request.query_params.get("end")
-        if not (start and end):
-            return Response(
-                {"error": "'start' or/and 'end' query params are missing"},
-                status=400,
-            )
-        if not (trydatetime(start) and trydatetime(end)):
-            return Response(
-                {
-                    "error": f"Your query params {start} or {end} do not match datetime format {settings.DATETIME_FORMAT}"
-                },
-                status=400,
-            )
+
+        if resp := return_drf_400_response_on_rangeparams_error(start, end):
+            return resp
 
         queryset = self.filter_queryset(self.get_queryset())
         serializer = self.get_serializer(
--- backend/urls.py
+++ backend/urls.py
@@ -2,13 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from .shifts.views import ShiftCalendarSpecificView, ShiftView
+from .shifts.views import ShiftView
 from .workers.views import WorkerView
 
 router = routers.DefaultRouter()
 router.register("workers", WorkerView)
 router.register("shifts", ShiftView)
-router.register("calendar", ShiftCalendarSpecificView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
